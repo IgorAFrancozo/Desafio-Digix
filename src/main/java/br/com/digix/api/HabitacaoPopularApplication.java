f7dc6af53e182d4cfe063db2c4987e669c88d454
Igor Françozo <igorfrancozo9@gmail.com>
2023-05-23 16:30:46 -0400

    Início do Projeto

---
+++ src/main/java/br/com/digix/api/HabitacaoPopularApplication.java
@@ -0,0 +1,13 @@
+package br.com.digix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitacaoPopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitacaoPopularApplication.class, args);
+	}
+
+}
