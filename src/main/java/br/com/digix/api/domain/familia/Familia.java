bb2c209317d2b94d5a3d0484658a2542ad90fc02
Igor Françozo <igorfrancozo9@gmail.com>
2023-05-24 17:28:10 -0400

    last

--- src/main/java/br/com/digix/api/domain/familia/Familia.java
+++ src/main/java/br/com/digix/api/domain/familia/Familia.java
@@ -1,10 +1,10 @@
 package br.com.digix.api.domain.familia;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Familia {
--- src/test/java/br/com/digix/api/service/CalculadoraPontosTest.java
+++ src/test/java/br/com/digix/api/service/CalculadoraPontosTest.java
@@ -1,4 +1,62 @@
 package br.com.digix.api.service;
 
+import br.com.digix.api.domain.familia.Familia;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
 public class CalculadoraPontosTest {
+
+    @Test
+    public void calcularPontuacao_RendaAte900_PontuacaoCorreta() {
+        // Arrange
+        Familia familia = new Familia();
+        familia.setRendaTotal(900);
+
+        // Act
+        PontuacaoFamilia pontuacao = CalculadoraPontos.calcularPontuacao(familia);
+
+        // Assert
+        assertThat(pontuacao.getPontuacao()).isEqualTo(5);
+    }
+
+    @Test
+    public void calcularPontuacao_RendaEntre901e1500_PontuacaoCorreta() {
+        // Arrange
+        Familia familia = new Familia();
+        familia.setRendaTotal(1200);
+
+        // Act
+        PontuacaoFamilia pontuacao = CalculadoraPontos.calcularPontuacao(familia);
+
+        // Assert
+        assertThat(pontuacao.getPontuacao()).isEqualTo(3);
+    }
+
+    @Test
+    public void calcularPontuacao_Dependentes3OuMais_PontuacaoCorreta() {
+        // Arrange
+        Familia familia = new Familia();
+        familia.setRendaTotal(2000);
+        familia.setDependentes(4);
+
+        // Act
+        PontuacaoFamilia pontuacao = CalculadoraPontos.calcularPontuacao(familia);
+
+        // Assert
+        assertThat(pontuacao.getPontuacao()).isEqualTo(3);
+    }
+
+    @Test
+    public void calcularPontuacao_DependentesEntre1e2_PontuacaoCorreta() {
+        // Arrange
+        Familia familia = new Familia();
+        familia.setRendaTotal(2000);
+        familia.setDependentes(2);
+
+        // Act
+        PontuacaoFamilia pontuacao = CalculadoraPontos.calcularPontuacao(familia);
+
+        // Assert
+        assertThat(pontuacao.getPontuacao()).isEqualTo(2);
+    }
 }
